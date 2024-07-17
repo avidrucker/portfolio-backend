bfe86f6ae600d1f81cdc9ad298dee2c49b08bfd3
Avi Drucker <6962664+avidrucker@users.noreply.github.com>
2024-07-17 01:07:18 +0000

    feat cors, dev. exclusion on page count

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
 let visitCounter = 0;
 
-app.get('/count', (req, res) => {
-  visitCounter++;
+app.use(cors({ origin: '*' }));
+
+app.get('/api', (req, res) => {
+  console.log("Bop");
+  res.send("In the future, I will say what the API can do here.");
+});
+
+app.get('/api/count', (req, res) => {
+  // if req is from dev.adrucker.com, then don't increment the counter
+  if (req.hostname !== 'dev.adrucker.com') {
+    visitCounter++;
+  }
   res.send(`Page has been visited ${visitCounter} times`);
 });
 
